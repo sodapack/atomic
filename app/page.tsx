f12aa19aa42708a802577e02274f4e1d1cc9824e
Ciril Joby <150200936+sodapack@users.noreply.github.com>
2025-05-24 14:19:43 -0400

    remove unused UI components and implement resizable and collapsible features

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
-import { ResizeableSidebarInset, ResizeableSidebarProvider } from "@/components/resizeable-sidebar"
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+
 import { AppSidebar } from "@/components/app-sidebar"
 
 export default function Page() {
   return (
-    <ResizeableSidebarProvider>
-      <AppSidebar />
-      <ResizeableSidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-        </header>
-      </ResizeableSidebarInset>
-    </ResizeableSidebarProvider>
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+    <ResizablePanelGroup
+      direction="horizontal"
+    >
+      <ResizablePanel defaultSize={25}>
+        <AppSidebar/>
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={75}>
+        <div className="flex h-full items-center justify-center p-6">
+          <span className="font-semibold">Content</span>
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+    </div>
   )
 }
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,19 +1,9 @@
 // import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
 
-import {
-  ResizeableSidebar,
-  ResizeableSidebarContent,
-} from "@/components/resizeable-sidebar"
-
-import { Checkbox } from "@/components/ui/checkbox"
 
 
 export function AppSidebar() {
   return (
-    <ResizeableSidebar>
-
-      <ResizeableSidebarContent>
-      </ResizeableSidebarContent>
-    </ResizeableSidebar>
+    <p>Yes</p>
   )
 }
