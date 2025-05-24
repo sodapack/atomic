34d45357ddc04745f26b55198fd929e773e827db
Ciril Joby <150200936+sodapack@users.noreply.github.com>
2025-05-24 12:28:02 -0400

    Make a copy of Sidebar component to make resizeable

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { ResizeableSidebarInset, ResizeableSidebarProvider } from "@/components/resizeable-sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 
 export default function Page() {
   return (
-    <SidebarProvider>
+    <ResizeableSidebarProvider>
       <AppSidebar />
-      <SidebarInset>
+      <ResizeableSidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
         </header>
-      </SidebarInset>
-    </SidebarProvider>
+      </ResizeableSidebarInset>
+    </ResizeableSidebarProvider>
   )
 }
